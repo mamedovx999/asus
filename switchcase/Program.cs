93ef27d73884fffbf5da103d8aac52fe220cb2e5
mamedovx999 <mammadalifm@code.edu.az>
2024-02-15 18:48:36 +0400

    First commit

---
+++ switchcase/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.WriteLine("Hello, World!");
+#region task1
+//Console.WriteLine("qapinin olculerin daxil et");
+//int qapieni = Convert.ToInt32(Console.ReadLine());
+//int qapihundurluk = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("skafin olculerin daxilet");
+//int skafen = Convert.ToInt32(Console.ReadLine());
+//int skafhundurluk = Convert.ToInt32(Console.ReadLine());
+//int skafuzunluq = Convert.ToInt32(Console.ReadLine());
+
+//if (qapieni > skafen && qapihundurluk > skafhundurluk || qapihundurluk < skafuzunluq)
+//{
+//    Console.WriteLine("qapidankecir");
+//}
+//else if (skafuzunluq < qapieni && skafen > qapihundurluk)
+//{
+//    Console.WriteLine("qapidankecir");
+//}
+//else
+//{
+//    Console.WriteLine("qapidankecmir");
+//}
+#endregion
+#region tas2
+//int n = 13;
+//int quvvet = 1;
+
+//while (quvvet<n)
+//{
+//    quvvet *= 2;
+//} 
+//if (quvvet == n)
+//{
+//    Console.WriteLine("2quvvetidir");
+//}
+//else
+//{
+//    Console.WriteLine("2quvvetideil");
+//}
+#endregion
+#region task3
+//int num = 1689;
+//int count = 0;
+//while (num!=0)
+//{
+//    count++;
+//    num /= 10;
+//}
+//Console.WriteLine(count);
+#endregion
